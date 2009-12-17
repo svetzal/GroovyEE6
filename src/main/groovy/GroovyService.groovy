a3137384a80d90322bf1af44665581b394b17ae7
Steven Vetzal <steve@vetzal.com>
2009-12-17 14:19:46 -0500

    d'oh! should have used inference for String type

--- src/main/groovy/GroovyService.groovy
+++ src/main/groovy/GroovyService.groovy
@@ -4,7 +4,7 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean(name="groovyBean")
 @RequestScoped
 class GroovyService {
-    String simpleProperty = "Nope, this is not a method!"
+    def simpleProperty = "Nope, this is not a method!"
 
     def getMessage() {
         "Hello, Groovy World!"
